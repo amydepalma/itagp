f56931cb827071b6ff523886046f653751d11cc8
Amy DePalma <amydepalma@gmail.com>
2024-02-10 02:02:03 -0500

    Initial commit

---
+++ itagp.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Is That a Good Price?
+* Plugin URI: https://itagp.amydepalma.com
+* Description: A personal price tracker.
+* Version: 1.0
+* Author: Amy DePalma
+* Author URI: https://www.amydepalma.com
+**/
